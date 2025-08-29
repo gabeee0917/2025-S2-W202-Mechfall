1a7bd8aaa8f7f09f5b47d94fa089eecb5e7c8088
Chang-Ho-Kim <fwr3521@autuni.ac.nz>
2025-08-29 22:22:20 +1200

    Chang Ho's progress as of Day 5
    Some edits, addition of sound to startup page and general tidying up of code names.
    Changed the font of input fields in login page back to liberationsans as the thaleah fat font only shows capitals. Other UI is fine using it.
    
    User stories still awaiting testing.

--- Mechfall/Assets/StartSceneSwitch.cs
+++ Mechfall/Assets/StartSceneSwitch.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class SceneSwitcher : MonoBehaviour
+public class StartSceneSwitcher : MonoBehaviour
 {
   public GameObject beam;
   public Material change;
 
+  public AudioSource beamsound;
   public void LoadLogin()
   {
     beam.SetActive(true);
     Image image = gameObject.GetComponent<Image>();
     image.material = change;
-    StartCoroutine(Delay(1));
-    
+    StartCoroutine(Delay(2));
+
   }
 
   IEnumerator Delay(float n)
   {
     yield return new WaitForSeconds(n);
     SceneManager.LoadScene("Login");
-  }    
+  }
+
+  public void PlayBeamSound()
+  {
+    beamsound.Play();  
+  }
         
 }
 
