f05e65b526486d1789d0bd626abffdbe5271a80f
TeOne-S <teonecshearer@gmail.com>
2025-09-18 18:55:22 +1200

    updated enemy to recat to getting hit
    when the enemy is hit they now jump up

--- Mechfall/Assets/Enemy.cs
+++ Mechfall/Assets/Enemy.cs
@@ -1,14 +1,21 @@
+using System.Reflection.Emit;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public int hp = 5;
-
+    public Rigidbody2D rb;
 
     public void TakeDamage(int d)
     {
         // Makes the object take damage and removes it if its hp is 0
         hp -= d;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+
+        Vector2 hitBack = (Vector2.up * (d * 2)) - ((Vector2)transform.right * d);
+
+        rb.AddForce(hitBack, ForceMode2D.Impulse);
 
         if (hp <= 0)
         {
