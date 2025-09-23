09e004419eae92273072c81dc92e3444a0abbea5
tedd27 <msr2587@autuni.ac.nz>
2025-09-24 11:10:52 +1200

    Created SpawnManager Script + Adjusted Feet Collider Size
    Spawn Locations now Work when transporting to different stage, Adjusted Portal Script to more appropriately just load scenes and adjusted feet collider size to avoid sticking to walls

--- Mechfall/Assets/Scripts/portal.cs
+++ Mechfall/Assets/Scripts/portal.cs
@@ -1,47 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Portal : MonoBehaviour
+public class portal : MonoBehaviour
 {
     private bool playerInRange = false;
     private GameObject player;
-
     public string scenename;
-
-
-
-    void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
 
     void Update()
     {
         if (playerInRange && InputManager.PlayerInput.actions["Interact"].triggered)
         {
-            player = GameObject.FindWithTag("Player");
             SceneManager.LoadScene(scenename); 
         }
     }
-
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (player != null)
-        {
-            GameObject spawn = GameObject.Find("PlayerSpawn");
-            if (spawn != null)
-            {
-                player.transform.position = spawn.transform.position;
-            }
-        }
-    }
-
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
