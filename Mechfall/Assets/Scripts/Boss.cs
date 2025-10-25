8244e26a3c52e274581643c2860355a495248cc4
TeOne-S <teonecshearer@gmail.com>
2025-10-26 04:58:39 +1300

    added phase 1 logic
    logic for phase 1 mechanics implimented

--- Mechfall/Assets/Scripts/Boss.cs
+++ Mechfall/Assets/Scripts/Boss.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using System.Reflection.Emit;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Boss : MonoBehaviour
 {
@@ -21,12 +23,18 @@ public class Boss : MonoBehaviour
 
     void Start()
     {
+        // Testing Functions
         //Instantiate(fallingObject, leftP.position, leftP.rotation);
         //Instantiate(fallingObject, rightp.position, rightp.rotation);
         //Instantiate(sideObject, rightC.position, rightC.rotation).GetComponent<ClosingBox>().right = false;
         //Instantiate(sideObject, leftC.position, leftC.rotation);
-        rightFall();
-        
+        //rightFall();
+        //leftFall();
+        //leftClose();
+        //rightClose();
+        //StartCoroutine(spawnEnemyLeft());
+        //StartCoroutine(spawnEnemyRight());
+
         hitable = false;
         phaseLogic();
     }
@@ -54,7 +62,7 @@ public class Boss : MonoBehaviour
         }
 
         phase += 1;
-        
+
         phaseLogic();
     }
 
@@ -66,7 +74,27 @@ public class Boss : MonoBehaviour
 
     void phaseLogic()
     {
+        if (phase == 1)
+        {
+            Invoke(nameof(phaseOne), 0);
+            Invoke(nameof(phaseOne), 8);
+            Invoke(nameof(phaseOne), 16);
+        }
+    }
 
+    void leftFall()
+    {
+        Vector2 position = new Vector2();
+        for (float i = 2.5f; i <= 8.5f; i++)
+        {
+            position = new Vector2(-i, -3.6f);
+            Instantiate(warning, position, rightC.rotation);
+        }
+        Invoke(nameof(summonLeftFall), 1f);
+    }
+    void summonLeftFall()
+    {
+        Instantiate(fallingObject, leftP.position, leftP.rotation);
     }
 
     void rightFall()
@@ -83,4 +111,95 @@ public class Boss : MonoBehaviour
     {
         Instantiate(fallingObject, rightp.position, rightp.rotation);
     }
+
+    void leftClose()
+    {
+        Vector2 position = new Vector2();
+        for (float i = 0.5f; i <= 8.5f; i++)
+        {
+            for (float j = 4.6f; j >= -3.6f; j--)
+            {
+                position = new Vector2(-i, j);
+                Instantiate(warning, position, rightC.rotation);
+            }
+        }
+        Invoke(nameof(summonLeftClose), 1f);
+    }
+    void summonLeftClose()
+    {
+        Instantiate(sideObject, leftC.position, leftC.rotation);
+    }
+
+    void rightClose()
+    {
+        Vector2 position = new Vector2();
+        for (float i = 0.5f; i <= 8.5f; i++)
+        {
+            for (float j = 4.6f; j >= -3.6f; j--)
+            {
+                position = new Vector2(i, j);
+                Instantiate(warning, position, rightC.rotation);
+            }
+        }
+        Invoke(nameof(summonRightClose), 1f);
+    }
+    void summonRightClose()
+    {
+        Instantiate(sideObject, rightC.position, rightC.rotation).GetComponent<ClosingBox>().right = false;
+    }
+
+    private IEnumerator spawnEnemyLeft()
+    {
+        GameObject enemySpawned = Instantiate(enemy, eLeft.position, eLeft.rotation);
+        enemySpawned.GetComponent<Enemy_Chaseing>().range = 100;
+        enemySpawned.GetComponent<Enemy_Chaseing>().speed = 1;
+        enemySpawned.GetComponent<Rigidbody2D>().gravityScale = 50;
+
+        yield return new WaitForSeconds(1);
+        enemySpawned.GetComponent<Rigidbody2D>().gravityScale = 1;
+        Debug.Log("ran");
+    }
+
+    private IEnumerator spawnEnemyRight()
+    {
+        GameObject enemySpawned = Instantiate(enemy, eRight.position, eRight.rotation);
+        enemySpawned.GetComponent<Enemy_Chaseing>().range = 100;
+        enemySpawned.GetComponent<Enemy_Chaseing>().speed = 1;
+        enemySpawned.GetComponent<Rigidbody2D>().gravityScale = 50;
+
+        yield return new WaitForSeconds(1);
+        enemySpawned.GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+    
+    void phaseOne()
+    {
+        int current = Random.Range(1, 5);
+        for (int i = 0; i <=4; i+=4)
+        {
+            switch (current)
+            {
+                case 1:
+                    Invoke(nameof(rightFall), i);
+                    break;
+                case 2:
+                    Invoke(nameof(rightClose), i);
+                    break;
+                case 3:
+                    Invoke(nameof(leftFall), i);
+                    break;
+                case 4:
+                    Invoke(nameof(leftClose), i);
+                    break;
+                default:
+                    Debug.Log("out of range");
+                    break;
+            }
+            if (current <= 2)
+            {
+                current = Random.Range(3, 5);
+            } else current = Random.Range(1, 3);
+        }
+        
+    }
+
 }
